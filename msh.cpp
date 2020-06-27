de04454e74f1a4fdf10613a67ba55db588aa5255
jpinasanz <jpinasanz@sdsu.edu>
2020-06-26 18:42:39 -0700

    latest updates

--- msh.cpp
+++ msh.cpp
@@ -3,7 +3,9 @@
 // microshell contstructor
 // pull in all necessary variables for running 
 msh::msh(void){
+    char cwd[1024];
     currentUser = getenv("USER");
+    currentDir = getcwd(cwd, sizeof(cwd));
 }
 
 // clear terminal, print welcome statement for the user
@@ -14,11 +16,26 @@ void msh::initShell(){
     sleep(1);
 }
 
-//run terminal prompt
-void msh::recieveInput(){
-    char* buf;
-    buf = 
+// pass through user input and return 
+int msh::recieveInput(char *userInput){
+    //create an inputBuffer for the direct user input
+    char* inputBuf;
+    string outLine = "\n"+ currentUser + ":" + currentDir + "%";
+    //read into the buffer the user line with the prompt:
+    //cssc9999:<current working directory>%
+    inputBuf = readline(outLine);
 
+    // if there is input in the inputBuffer then we will add it to the 
+    // command history so it may be referenced later
+    // we then save the inputBuffer to userInput
+    if (strlen(inputBuf) != 0){
+        add_history(inputBuf);
+        strcpy(userInput, inputBuf);
+        return 0;
+    }
+    else{
+        return 1;
+    }
 }
 
 
@@ -26,6 +43,13 @@ void exitShell(){
     exit(0);
 }
 
-void cmdLineLoop(){
+void msh::cmdLineLoop(){
+    initShell();
+
+    while(1){
+        if(recieveInput(userInput)){
+            continue;
+        }
+    }
     
 }
--- msh.h
+++ msh.h
@@ -2,30 +2,37 @@
 #define MSH_H
 
 #include "FileValidate.h"
-#include "FileHandler.h"
+//#include "FileHandler.h"
 // ^ already includes: 
 //  <string.h>
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h> 
+#include <readline/history.h>
+
+#define MAXCHAR 256;
 
 class msh{
     public:
 
         //public variables
         string currentUser;
+        string currentDir;
+        char userInput[MAXCHAR];
 
         // public constructor
         msh();
 
         //public function
-
+        void cmdLineLoop()
     private:
         //private variables
         
         //private functions
         void initShell();
-        void recieveInput();
+        int recieveInput();
 
 };
 
