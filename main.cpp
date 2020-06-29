0d86f18fb13ce50438bf00bbc0eb1612a811d1b5
CS-Buran <62851064+CS-Buran@users.noreply.github.com>
2020-06-29 06:13:16 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,22 +8,22 @@ Filename: main.cpp
 
 #include "main.h"
 
-int main() {                                                //takes and runs userInput until the user enters the 'quit' command
+int main() {                                                          //takes and runs userInput until the user enters the 'quit' command
   char *userInput[MAX_ARGS], *lhBufF[MAX_ARGS], *rhBuff[MAX_ARGS];
   PipeStatus pipe_status;
   int argc;
 
   while (1) {
-                                                                       // Reads in the command from the user.
+                                                                      // Reads in the command from the user.
     argc = readArgs(userInput);
     pipe_status = parse_command(argc, userInput, lhBuff, rhBuff);     //reads the userInput and decides if it is necissary to split into left and rigth buffs.
-                                                                      //will give a pipe or nopipe enum
+                                                                      //will give a PIPE or NOPIPE enum
     if (pipe_status == PIPE)                                          //PIPE
       pipeCmd(lhBuff, rhBuff);
     else
       runCmd(argc, userInput);                                        //NOPIPE
-    // Reset the argv array for next time.
-    for (int i=0; i<argc; i++)
+                                                                      
+    for (int i=0; i<argc; i++)                                        //resets userInput for the next input
       auserInput[i] = NULL;
   }
   return 0;
